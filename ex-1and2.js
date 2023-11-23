e0722ea1e8f49026444c7782007bc86edd6e1472
nattawat <frank.nattawat@hotmail.com>
2023-11-23 14:13:21 +0700

    feat:complete ex variable

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let fristName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const totalCost = 15000;
+const totalRestaurantActive = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 country Rd#4Meeker , Colorado(CO), 81641";
+totalCost = 5000;
+totalRestaurantActive = 2500;
+
